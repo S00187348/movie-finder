26a1ee49eb844fdde3df90b262f8e3f5a1e4b1bb
cinlea <46897273+cinlea@users.noreply.github.com>
2021-03-27 00:51:18 +0000

    Add files via upload

---
+++ services/omdb-api.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Observable} from 'rxjs';
+import {catchError,tap} from 'rxjs/operators';
+import {IOMDBResponse} from '../omdbresponse';
+@Injectable({
+  providedIn: 'root'
+})
+export class OmdbApiService {
+
+  private _siteURL="https://www.omdbapi.com/";
+  private _key='?apikey=xxxxxxxx&t=';
+  constructor(private _http:HttpClient){ }
+  GetMovieData(movieName): Observable<IOMDBResponse>{
+    return this._http.get<IOMDBResponse>(this._siteURL + this._key + movieName)
+    .pipe(
+      tap(data => console.log('Moviedata/error' + JSON.stringify(data))
+      ),
+      catchError(this.handleError)
+    );
+  }
+  private handleError(err:HttpErrorResponse){
+    console.log('OmdbApiService:' + err.message);
+    return Observable.throw(err.message);
+  }
+
+}
